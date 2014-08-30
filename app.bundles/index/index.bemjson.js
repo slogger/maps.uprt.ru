2b01bd8915fe188774a523da815eb9d57bfe69f3
Maxim Syrykh <slogger1994@gmail.com>
2014-08-31 00:27:22 +0600

    CSScombed for css ymaps api work map create with geolocation

--- app.bundles/index/index.bemjson.js
+++ app.bundles/index/index.bemjson.js
@@ -15,8 +15,28 @@
             js: true,
         },
         {
-            block: 'footer'
-        },
-       '<!-- Yandex.Metrika counter --><script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter25978051 = new Ya.Metrika({id:25978051, webvisor:true, clickmap:true}); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/25978051" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->'
+            block: 'search',
+            js: true,
+            content: [
+                {
+                    block: 'input',
+                    // mods : { theme : 'material', type : 'search'},
+                    placeholder : 'Куда вам надо?'
+                },
+                {
+                    block: 'button',
+                    content: [
+                        'Поиск'
+                    ]
+                },
+                {
+                    block: 'button',
+                    content: [
+                        'Фильтр'
+                    ]
+                }
+            ]
+        }
+        // '<!-- Yandex.Metrika counter --><script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter25978051 = new Ya.Metrika({id:25978051, webvisor:true, clickmap:true}); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/25978051" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->'
     ]
 })
---
+++ common.blocks/map/map.browser.js
@@ -0,0 +1,68 @@
+modules.define('map', ['i-bem__dom', 'loader_type_js', 'jquery'], function(provide, DOM, loader, $) {
+
+    DOM.decl('map', {
+        onSetMod: {
+           /**
+            * Выполняется при инициализации блока
+            */
+            'js': {
+                'inited': function() {
+                    this._loadYmapsApi();
+                }
+            },
+
+           /**
+            * Выполняется после загрузки API Яндекс.Карт
+            */
+            'ymapsApiLoaded': function() {
+                this._drawMap()
+            }
+        },
+
+        _loadYmapsApi: function() {
+            var self = this;
+
+            loader('//api-maps.yandex.ru/2.1/?lang=ru_RU&load=package.full', function() {
+                self.setMod('ymapsApiLoaded');
+            });
+        },
+
+        _drawMap: function() {
+            var self = this;
+
+            ymaps.ready(function() {
+                ymaps.geolocation.get().then(function (res) {
+                    var mapContainer = $('#map'),
+                        bounds = res.geoObjects.get(0).properties.get('boundedBy'),
+                        mapState = ymaps.util.bounds.getCenterAndZoom(
+                            bounds,
+                            [mapContainer.width(), mapContainer.height()]
+                        );
+                    mapState.controls = [];
+                    createMap(mapState);
+                }, function (e) {
+                    // Если место положение невозможно получить, то просто создаем карту.
+                    createMap({
+                        center: [55.751574, 37.573856],
+                        zoom: 2,
+                        controls: []
+                    });
+                });
+                
+                function createMap (state) {
+                    self._map = new ymaps.Map('map', state);
+                }
+            });
+
+        },
+
+        /**
+         * @return {Map | Null} Экземпляр карты, либо null, если карта не инстанцирована.
+         */
+        getMap: function () {
+            return this._map || null;
+        }
+    });
+
+    provide(DOM);
+});
